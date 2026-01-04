f4d87a07a879d0f95e03a94708543e43378b7012
Moussa Mokhtari <me@moussamokhtari.com>
2026-01-04 04:08:02 +0300

    feat: Midnight Aurora Theme v0.0.1

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,11 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
   console.log('Extension "Midnight Aurora Theme" is now active!');
 
-  // Register your commands and providers here
+  const disposable = vscode.commands.registerCommand('midnight-aurora-theme.helloWorld', () => {
+    vscode.window.showInformationMessage('Hello from Midnight Aurora Theme!');
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
